df38819cd8bc4d64f5b7972f78f0fbfb74965979
Sambit Praharaj <sambit_praharaj@yahoo.com>
2016-11-03 16:08:59 +0100

    Update test.js
    Added additional functionalities and refined the code

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
 // Declare the arrays globally
+
+// Variables to store the event types, date and time, processed date and time and time in seconds
+
 var events = [];
 var time_raw = [];
 var users = [];
@@ -6,7 +9,7 @@ var date = [];
 var time = [];
 var time_seconds = [];
 
-// These variables store the binary state of each user for a certain time interval
+// These variables store the binary state of each user as a string for a certain time interval
 
 var exercise = [];
 var connected = [];
@@ -14,15 +17,24 @@ var focus = [];
 var idle = [];
 var input = [];
 var submitted = [];
+
+// Variables store all the students using ASQ in a time duration, the number state of each student also their category
+
 var students = [];
+var number = [];
+var category = [];
 var y = 0;
 
 $.getJSON('sessionevents.json', function(info){
-	// Collect the events, raw time (i.e. time with date) and the user ids.
+
+	// Collect the events, raw time (i.e. time with date) and the user ids
+
    		   	for(var i = 0; i < info.length; i++){
    		   		events[i] = info[i].type;
    		   		time_raw[i] = info[i].time.$date;
-   		   		// The user id can be obtained from in data as user, or object or in data as an answeree
+
+   		   		// The user id can be obtained in data as user, or object or in data as an answeree
+
    		   		if(info[i].data.user === undefined){
    		   		    users[i] = info[i].data.answeree.$oid;
    		   		}
@@ -85,32 +97,42 @@ function process()
   {
     for(var l = 0; l < time_raw.length; l++){
 
+      // To check for idle user
+
+        if(events[l] === "viewer-idle"){
+         idle[index] = "1";
+         continue;
+        }
+        if(events[l] === "tabhidden" || events[l] === "tabvisible" || events[l] === "windowfocus" || events[l] === "windowblur" || events[l] === "focusin" || events[l] === "focusout" || events[l] === "exercisefocus" || events[l] === "exerciseblur" || events[l] === "input" || events[l] === "questioninput" || events[l] === "exercise-submit" || events[l] === "answer-submitted"){
+         idle[index] = "0";
+        }
+
       // To check for connected user
 
       if(events[l] === "folo-connected"){
-         connected[index]++;
+         connected[index] = "1";
          continue;
       } // fix the index value
       if(events[l] === "folo-disconnected"){
-         connected[index] = 0;
+         connected[index] = "0";
          continue;
       }
       
       // To check if a user is in an exercise
 
       if(events[l] === "exercise-activated"){
-            exercise[index]++;
+            exercise[index] = "1";
             continue;
         }
         if(events[l] === "exercise-deactivated"){
-         exercise[index] = 0;
+         exercise[index] = "0";
          continue;
         }
 
         // To check for user focus
 
         if(events[l] === "windowfocus" || events[l] === "exercisefocus"){
-            focus[index]++;
+            focus[index] = "1";
             continue;
         }
         /*if(events[l] === "windowblur" || events[l] === "exerciseblur"){
@@ -121,36 +143,48 @@ function process()
         // To check for user input
 
         if(events[l] === "input" || events[l] === "questioninput"){
-         input[index]++;
+         input[index] = "1";
          continue;
         }
 
         // To check for user submission
 
         if(events[l] === "exercise-submit"){
-         submitted[index]++;
-         continue;
-        }
-
-        // To check for idle user
-
-        if(events[l] === "viewer-idle"){
-         idle[index]++;
-         continue;
-        }
-        if(events[l] === "tabhidden" || events[l] === "tabvisible" || events[l] === "windowfocus" || events[l] === "windowblur" || events[l] === "focusin" || events[l] === "focusout" || events[l] === "exercisefocus" || events[l] === "exerciseblur" || events[l] === "input" || events[l] === "questioninput" || events[l] === "exercise-submit" || events[l] === "answer-submitted"){
-         idle[index] = 0;
+         submitted[index] = "1";
          continue;
         }
     }
   }
   
-  // Converts the binary state of six indicators to decimal values which indicate different annotated states
+  // Converts the binary state of six indicators to decimal values
 
   function IndicatorstoNumbers()
   {
-   for(var x = 0; x < index.length; x++){
+   for(var x = 0; x < students.length; x++){
+      number[x] = parseInt((submitted[x]+input[x]+idle[x]+focus[x]+connected[x]+exercise[x]),2);
+   }
+  }
+
+  // Assign students to different categories
+
+  function Categories()
+  {
+   for(var y = 0; y < students.length; y++){
       
+      // 1 for in-attentive, 2 for semi-attentive and 3 for attentive categories
+
+      if(number[y] === 0 || number[y] === 1 || number[y] === 33 || number[y] === 2 || number[y] === 10 || number[y] === 35 || number[y] === 43){
+         category[y] = 1;
+         continue;
+      }
+      if(number[y] === 11 || number[y] === 14 || number[y] === 15 || number[y] === 47){
+         category[y] = 2;
+         continue;
+      }
+      if(number[y] === 3 || number[y] === 6 || number[y] === 7 || number[y] === 39 || number[y] === 23 || number[y] === 55){
+         category[y] = 3;
+         continue;
+      }
    }
 
   }
@@ -175,4 +209,6 @@ for(var j = 0; j < time_raw.length; j++){
    index = events.indexOf("exercise-activated");
   }
 }*/
-// Yet to complete
+
+
+// Yet to Complete
